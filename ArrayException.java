f841fa73310c7d4ac069a9fe1992f4e39a3f9d99
GabrielOcta <79490346+GabrielOcta@users.noreply.github.com>
2021-06-05 14:05:48 +0700

    Add files via upload

---
+++ ArrayException.java
@@ -0,0 +1,11 @@
+package com.Gabriel.exceptions;
+
+/**
+ * @Author 1972037 Gabriel Octa Mahardika
+ **/
+public class ArrayException extends Exception {
+
+    public ArrayException(String message) {
+        super(message);
+    }
+}
